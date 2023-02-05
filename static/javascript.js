bfda87cd6ffaa1c8c03c1a395309a2d719285cc8
Mia <blueria10@gmail.com>
2023-02-05 13:49:44 -0500

    Add existing file

---
+++ static/javascript.js
@@ -0,0 +1,9 @@
+function myFunc(){
+    var x = document.getElementById("mytopnav");
+    if(x.className==="topnav"){
+        x.className += " responsive";
+    }
+    else{
+        x.className = "topnav"
+    }
+}
---
+++ templates/javascript.js
@@ -0,0 +1,9 @@
+function myFunc(){
+    var x = document.getElementById("mytopnav");
+    if(x.style.display==="block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block"
+    }
+}
