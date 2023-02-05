8274acba899fdafe2cf850d2628116adac8faa62
Emmanuella Desruisseaux <85198940+blueria10@users.noreply.github.com>
2023-02-05 13:56:02 -0500

    Delete javascript.js

--- templates/javascript.js
+++
@@ -1,9 +0,0 @@
-function myFunc(){
-    var x = document.getElementById("mytopnav");
-    if(x.style.display==="block"){
-        x.style.display = "none";
-    }
-    else{
-        x.style.display = "block"
-    }
-}
